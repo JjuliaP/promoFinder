b8e9036205aee0c03ef373c6ecb998b43ff54463
jJuliaP <juli.pavliv@gmail.com>
2018-12-02 15:20:02 +0200

    Add all files

---
+++ src/components/promoInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <label for="input">{{ label }}</label>
+    <input type="text" name="gender" id="input" :placeholder="this.placeholder">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "promoInput",
+  props: {
+    label: String,
+    placeholder: String,
+    focus: Boolean
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
